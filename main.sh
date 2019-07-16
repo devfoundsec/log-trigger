0bb930f665df4fb7237a5a0453cf7b8a093b4464
jul10l1r4 <jul10l1r4@disroot.org>
2019-07-16 19:14:33 +0000

    :tada: Main tool

---
+++ main.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#-------------
+# Its free and open source at SCORPIONSEC
+# for issue and support github.com/SCORP1ON-TEAM/log-trigger/issues
+
+# Resolve directories
+here=`dirname "$0"`
+cd "$here"
+
+# Make env.
+[ ! -e /tmp/log-*.log ] && export tmp_log="$(mktemp /tmp/log-XXXXXX.log)" || export tmp_log="$(ls /tmp/log-*.log)"
+origin_log="$1"
+
+# Run Check and trigger
+export buff="$(diff $origin_log $tmp_log)"
+if [ "$buff" != "" ]
+then 
+    bash ./run.sh "$buff"
+    cp -f "$origin_log" "$tmp_log"
+fi
+
+unset -v $buff $origin_log $here $tmp_log
